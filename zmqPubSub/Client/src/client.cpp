a2f75e061039b72bf5f9001b7ad3e3c6909ae1f9
OxiSgen <60621483+OxiSgen@users.noreply.github.com>
2022-04-04 15:54:55 +0300

    add z1

---
+++ zmqPubSub/Client/src/client.cpp
@@ -0,0 +1,89 @@
+﻿#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/foreach.hpp>
+#include <iomanip>
+
+using std::setw;
+using std::cout;
+using std::endl;
+using std::cerr;
+
+
+void json_deserializer(boost::property_tree::ptree& pt ,
+                       zmq::message_t& message) {
+    std::stringstream jsonStr(message.to_string());
+    try {
+        boost::property_tree::read_json(jsonStr, pt);
+    }
+    catch (std::exception& e) {
+        cerr << e.what() << std::endl;
+    }
+}
+
+class Client {
+public:
+    Client(std::string port, std::string topic)
+        : port(port), zmq_topic(topic)
+    {}
+
+    ~Client() {
+        socket.close();
+        context.close();
+    }
+
+    void start_client() {
+        socket = { context, zmq::socket_type::sub };
+        std::cout << "Connecting to server..." << std::endl;
+        try {
+            socket.connect("tcp://zmq-server-cpp:" + port);
+        }
+        catch (zmq::error_t er) {
+            std::cout << "Что-то пошло не так. Ошибка при подключении к серверу..." << std::endl;
+        };
+        data_recieve(zmq_topic);
+    }
+
+    void data_recieve(std::string top) {
+        {
+            socket.setsockopt(ZMQ_SUBSCRIBE, top.data(), top.size());
+        }
+        zmq::message_t topic;
+        socket.recv(topic);
+        zmq::message_t message;
+        socket.recv(message);
+
+        printer(message);
+    }
+
+    void printer(zmq::message_t& message) {
+        boost::property_tree::ptree pt;
+        json_deserializer(pt, message);
+        pt.sort();
+
+        cout << setw(10) << "id" << setw(12) << "surname" << setw(12) << "name" << setw(17) << "date" << endl;
+        cout << "-------------------------------------------------------------" << endl;
+        BOOST_FOREACH(boost::property_tree::ptree::value_type & framePair, pt) {
+            cout << setw(10) << framePair.second.get<std::string>("id") << " |" <<
+                    setw(12) << framePair.second.get<std::string>("surname") << " |" <<
+                    setw(12) << framePair.second.get<std::string>("name") << " |" <<
+                    setw(17) << framePair.second.get<std::string>("date") << endl;
+            }
+    }
+
+private:
+    std::string zmq_topic;
+    zmq::socket_t socket;
+    std::string port;
+    zmq::context_t context;
+};
+
+
+int main()
+{
+    Client cl("5555", topic="students");
+    cl.start_client();
+    return 0;
+}
---
+++ zmqPubSub/Server/src/server.cpp
@@ -0,0 +1,169 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <zmq.hpp>
+#include <boost/algorithm/string/classification.hpp>
+#include <boost/algorithm/string/split.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <stdlib.h>
+#include <chrono>
+#include <thread>
+
+#ifdef _WIN32
+#include <conio.h>
+#include <windows.h>
+#define PRESSKEY() (!kbhit())
+#else
+#include <curses.h>
+#define PRESSKEY() (getch() == ERR)
+#endif // _WIN32
+
+
+struct Student {
+    std::string id;
+    std::string name;
+    std::string surname;
+    std::string date;
+};
+
+void json_serializer(boost::property_tree::ptree& pt, std::vector<Student>& st_vec) {
+    /* Сериализуем данные из файла в  Json, в котором ключи имеют вид "ФамилияИмяДата".
+    Так сразу отсеятся дублирующие записи и сортировка сведётся к тривиальной (по ключам). */
+    using namespace std;
+
+    for (auto& st : st_vec) {
+        string key = st.surname + st.name + st.date;
+        std::replace(key.begin(), key.end(), '.', '_');
+        if (!pt.count(key)) {
+            pt.put(key + ".id", st.id);
+            pt.put(key + ".name", st.name);
+            pt.put(key + ".surname", st.surname);
+            pt.put(key + ".date", st.date);
+        }
+    }
+}
+
+
+void file_read(std::vector<Student>& st, std::string FileName) {
+    std::string buffer;
+    std::vector<std::string> pv;
+
+    std::ifstream fin(FileName);
+    while (std::getline(fin, buffer)) {
+        boost::split(pv, buffer, boost::is_any_of(" "));
+        try {
+            st.push_back({ pv.at(0), pv.at(1), pv.at(2), pv.at(3) });
+        }
+        catch (std::out_of_range) {
+            continue;
+        }
+    }
+    fin.close();
+}
+
+void file_write(std::vector<Student>& st, std::string file_name) {
+    std::ofstream myfile;
+    myfile.open(file_name);
+    for (auto& s : st) {
+        myfile << s.id << " " << s.name << " " << s.surname << " " << s.date << '\n';
+    }
+    myfile.close();
+}
+
+
+class ServerSub
+{
+public:
+    explicit ServerSub(std::string port)
+        : port(port)
+    {}
+
+    ~ServerSub() {
+        socket.close();
+        context.close();
+    }
+
+    void startServer(std::string& json_str) {
+        zmq::context_t context(1);
+        try {
+            socket = { context, zmq::socket_type::pub }; // ZMQ_PUB
+            socket.bind("tcp://*:" + port);
+        }
+        catch (zmq::error_t) {
+            std::cout << "Что-то пошло не так. Ошибка при связывании сокета..." << std::endl;
+        }
+        handle(json_str);
+    }
+
+    void serverStop() {
+        socket.close();
+        context.close();
+    }
+
+    void filePrinter() {
+    }
+
+    void handle(std::string& json_str) {
+        std::cout << "Working... Для остановки сервера нажмите любую клавишу\n";
+        while (PRESSKEY()) {
+            //if (GetKeyState(VK_RETURN)) break;
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
+            zmq::message_t topic(static_cast<std::string>("students"));
+
+            std::string s = json_str;
+            zmq::message_t reply(s); 
+            try {
+                socket.send(topic, zmq::send_flags::sndmore);
+                socket.send(reply, zmq::send_flags::none);
+            }
+            catch (zmq::error_t) {
+                std::cout << "Ошибка при отправке сообщений" << std::endl;
+            }
+        }
+        serverStop();
+    }
+
+private:
+    zmq::socket_t socket;
+    std::string port;
+    zmq::context_t context;
+};
+
+
+void some_logic() {
+    ServerSub ser("5555");
+
+    std::string json_str;
+    // Читаем данные из файла
+    {
+        std::stringstream json;
+        std::vector<Student> st;
+        file_read(st, "file_student.txt");
+        file_read(st, "file_student_2.txt");
+
+        boost::property_tree::ptree data;
+        json_serializer(data, st);
+
+        try {
+            boost::property_tree::json_parser::write_json(json, data);
+        }
+        catch (std::exception& e) {
+            std::cerr << e.what() << std::endl;
+        }
+
+        file_write(st, "outfile/file_student_3.txt");
+        json_str = json.str();
+    }
+    ser.startServer(json_str);
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    some_logic();
+    return 0;
+}
